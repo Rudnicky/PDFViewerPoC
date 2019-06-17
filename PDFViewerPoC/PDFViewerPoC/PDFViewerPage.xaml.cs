e77c49471958f6eaa104fa0d71bc0ba5ba6aeee7
Pawel Rudnicki <pawel.rudnicki@thinkproject.com>
2019-06-17 10:28:06 +0200

    Add project files.

---
+++ PDFViewerPoC/PDFViewerPoC/PDFViewerPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDFViewerPoC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PDFViewerPage : ContentPage
+    {
+        public PDFViewerPage()
+        {
+            InitializeComponent();
+            InitializePdfViewerResource();
+        }
+
+        private void InitializePdfViewerResource()
+        {
+            pdfViewerControl.InputFileStream = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("PDFViewerPoC.xxx.pdf");
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            pdfViewerControl.Unload();
+        }
+    }
+}
